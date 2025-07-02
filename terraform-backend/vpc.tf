81548a941c52b77f96fa700a8c9fecdefca2fec4
Courtney Dahlson <courtneydahlson@gmail.com>
2025-07-02 15:44:39 -0400

    Created private route table

--- terraform-backend/vpc.tf
+++ terraform-backend/vpc.tf
@@ -59,6 +59,22 @@ resource "aws_nat_gateway" "nat" {
   depends_on = [aws_internet_gateway.igw]
 }
 
+# Creates a route table for the private subnets.
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+}
+
+# Route Table for Public Subnet
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+  tags   = { Name = "public-rt" }
+}
+
 # Associates the private subnets with the route table.
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.private_subnet_1.id
@@ -69,12 +85,6 @@ resource "aws_route_table_association" "b" {
   route_table_id = aws_route_table.private_rt.id
 }
 
-
-# Route Table for Public Subnet
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main.id
-  tags   = { Name = "public-rt" }
-}
 
 # Route: Allow outbound to internet via IGW
 resource "aws_route" "public_route" {
