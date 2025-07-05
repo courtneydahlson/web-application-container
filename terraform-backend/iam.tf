0bbf0bd64b1cbe03193dac8417359bc33c88bb07
Courtney Dahlson <courtneydahlson@gmail.com>
2025-07-05 16:48:21 -0400

    Created ecs and alb

--- terraform-backend/iam.tf
+++ terraform-backend/iam.tf
@@ -12,7 +12,7 @@ resource "aws_iam_role" "ecs_task_execution" {
     })
 }
 
-resource "aws_iam_role_policy_attachement" "ecs_task_execution_policy" {
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
     role = aws_iam_role.ecs_task_execution.name
     policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
